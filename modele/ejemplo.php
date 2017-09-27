1f7eae0782625d9bd3a9dee4eb4d38cb1eed3067
Emilio_Doria <Emilio_Doria@Emilio-Doria>
2017-09-27 12:40:18 +0200

    Projet

---
+++ modele/ejemplo.php
@@ -0,0 +1,56 @@
+<?php
+require 'connect.php';
+function getLesVols()
+{
+    
+// Déclaration d’un tableau
+$vols = array();
+
+  // Appel au fichier permettant la connection à la BD
+
+ $connexion= connect();
+			if ($connexion)	
+			{
+                            
+  // connexion réussie
+ // mysql_select_db("airazur",$connexion);
+ $vols=[];
+// Requete de selction des vols
+ $requete= "select numVol, aeroport.libelle, aeroport.pays, aeroport2.libelle, aeroport2.pays, dateDepart, heureDepart, dateArrivee, heureArrivee, prix from vol JOIN aeroport JOIN aeroport2 where vol.idDepart=aeroport.id AND vol.idArrivee=aeroport2.idArrivee" ;
+  $result=$connexion->query($requete);
+  while($row=$result->fetch(PDO::FETCH_ASSOC)){
+      $vols=$row;
+
+  }
+  
+     return $vols;
+	}
+
+else
+{
+  echo "problème lors de la connexion <br />";
+}
+mysql_close($connexion);
+
+
+
+return $vols;
+
+}
+
+
+print_r (getLesVols());
+/* 
+ * 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
